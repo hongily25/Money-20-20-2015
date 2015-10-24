d80484bee8ed171f7e0f4e316b180fb2422b4ae1
Emily Lam <hlam93@gmail.com>
2015-10-24 12:52:47 -0700

    files

---
+++ script.js
@@ -0,0 +1,45 @@
+
+		$(document).ready(function() {
+			
+			$('#minus-button').click(function() {
+				if ($('#minus-button img').attr('src') == 'images/minus.jpg') {
+        $('#minus-button img').attr('src','images/plus.jpg');
+					var test = $('.bigIMG').css('height') ;
+					//alert('test: ' + test);
+					$('img:not(.bigIMG)').toggle();
+					
+    } else {
+        $('#minus-button img').attr('src','images/minus.jpg');
+    }
+			
+				$('#wrapper').toggleClass("grid-img");
+				$('#container').toggleClass("shelf-img");
+				
+				
+		
+				
+			});
+			
+			$('.draggable').mouseup(function() {
+				
+				var offset = $(this).offset().left;
+				
+				
+			if(offset <= 700) {
+			//	alert('Regular offset: ' + offset);
+				$(this).addClass('bigIMG');
+				$(this).css('position','absolute');
+				$(this).css('height','initial');
+				$(this).css('width','initial');
+				var test2 = $(this).css('height');
+				//alert('test2: ' + test2);
+			}
+			else {
+				$(this).css('position','relative');
+				$(this).css('height','100px');
+				$(this).css('width','100px');
+				//alert('Else offset: ' + offset);
+			}
+		});
+			
+		});
